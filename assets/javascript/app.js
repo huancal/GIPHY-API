d9679ed1e88e50206094b3ca06b0eaaffe06dbc6
Huan Nguyen <huancal1984@gmail.com>
2019-10-25 13:12:21 -0700

    logic 90 percent done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,46 @@
 $(document).ready(function () {
 
-    // button click function
+    var buttonList = ['puppies', 'kittens', 'babies'];
 
-    $('button').on('click', function () {
+    function renderButtons() {
+        $('#button-area').empty();
+        for (var i = 0; i < buttonList.length; i++) {
+            var giphybtn = $("<button class='btn-outline-success m-3'>");
+            giphybtn.attr('data-search', buttonList[i]);
+            giphybtn.addClass('animalbtn');
+            giphybtn.text(buttonList[i]);
+            $('#button-area').append(giphybtn);
+        }
+    }
+
+    $('#add-gifs').on('click', function (e) {
+        e.preventDefault();
+        var textinput = $('#topic-input').val().trim();
+        console.log(textinput);
+        buttonList.push(textinput);
+        renderButtons();
+    })
+
+    renderButtons();
+
+    // Adding click event listeners to all elements with a class of "gifTastic"
+    $(document).on("click", ".animalbtn", displayGifs);
+
+    function displayGifs() {
+        $('#gifs-here').empty();
         // var for search subject 
-        var searchSubject = $(this).data('search');
+        var searchSubject = $(this).attr('data-search');
+        console.log(searchSubject);
+
+
+
 
         // API search endpoint
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchSubject + "&api_key=f02LsB2d5MOPCj9giwOpcqWQ6XSuI0Qh&limit=10&offset=0&rating=PG&lang=en"
+
+        var API_Key = "f02LsB2d5MOPCj9giwOpcqWQ6XSuI0Qh"
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchSubject + "&api_key=" + API_Key + "&limit=10&offset=0&rating=PG&lang=en"
+
         console.log(queryURL);
 
 
@@ -17,14 +50,41 @@ $(document).ready(function () {
                 method: 'GET',
             })
             .done(function (response) {
-                console.log(response.data[0].rating);
+
                 // looping through 10 gifs
                 for (var i = 0; i < response.data.length; i++) {
+                    console.log(response.data[i]);
+                    var animated = response.data[i].images.fixed_height.url;
+                    var still = response.data[i].images.fixed_height_still.url;
                     $('#gifs-here').append("<p>Rating: " + response.data[i].rating + "</p>")
-                    $('#gifs-here').append("<img src ='" + response.data[i].images.downsized.url + "'>");
-                };
+                    // $('#gifs-here').append("<img src ='" + response.data[i].images.downsized.url + "'>");
+
+                    var gifImage = $("<img>");
+
+                    gifImage.attr("src", still);
+                    gifImage.addClass('animateImg');
+                    gifImage.attr("data-still", still);
+                    gifImage.attr("data-animate", animated);
+                    gifImage.attr('data-state', 'still');
+                    $('#gifs-here').append(gifImage);
+                }
             })
+    }
+
+    //ON BUTTON CLICK ANIMATE OR STILL THE GIF IMAGES in case of dynamic buttons we added event listener after the $document is loaded
+    $(document).on('click', '.animateImg', function () {
+        var state = $(this).attr("data-state");
+        console.log(state);
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
     })
+
 
 
 
